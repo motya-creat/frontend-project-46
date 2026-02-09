f0dc7473f6de25f51baad13246d4b67cab65e819
Your Name <you@example.com>
2026-02-09 17:49:14 +0300

    add new logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+import { getFileData, getPath} from './utils.js'
+
+import getDifference from './getDIfference.js'
+import formater from './formatter/formatter.js'
+
+
 export default function getDiff(file1, file2, style='stylish') {
+    const filePatch1 = getPath(file1)
+    const filePatch2 = getPath(file2)
     
+    const fileData1 = getFileData(filePatch1)
+    const fileData2 = getFileData(filePatch2)
+
+    const diffData = getDifference(fileData1, fileData2)
+
+    return formater(diffData, style) 
 }
