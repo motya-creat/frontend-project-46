7aea72e12f74c7f41c8d03c081ad49ec053c08ad
Your Name <you@example.com>
2026-02-09 17:16:57 +0300

    feat: new func getUniq

--- src/utils.js
+++ src/utils.js
@@ -5,5 +5,28 @@ import parser from './parser.js'
 const getFormat = (filepath) => path.extname(filepath).slice(1)
 const getPath = (filepath) => path.resolve(process.cwd(), filepath)
 
+const getFileData = (filepath) => {
+    const format = getFormat(filepath)
+    const data = fstat.readFileSync(filepath, {encoding: 'utf-8'})
+    
+    return parser(data, format)
+}
 
-export {getFormat, getPath }
+const getUniq = (...arr) => {
+    const result = []
+    const existed = new Set()
+
+    arr.forEach((array) => {
+        array.forEach((val) => {
+            if(!existed.has(val)) {
+                result.push(val)
+            }
+            existed.add(val)
+        })
+    })
+
+    return result
+}
+
+
+export {getFormat, getPath, getFileData, getUniq }
